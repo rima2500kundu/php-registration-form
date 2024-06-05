ad2a672f3fd43b887f8ca9f0ebf173a1ec1bb0f5
Rima Kundu <rimasdgworks@gmail.com>
2024-06-05 19:16:23 +0530

    fixed previous error but still error shows

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $databaseName = 'tutorials';
 $databaseUser = 'root';
 $databasePassword = '';
 
-$mysqli = mysqli_connect($databaseHost, $databaseName, $databaseUser, $databasePassword);
+$mysqli = mysqli_connect($databaseHost, $databaseUser, $databasePassword, $databaseName); // keep maintain the squence otherwise there will be an error.
 
 // if(!$mysqli){
 //     die("error detected".mysqli_error($mysqli));
